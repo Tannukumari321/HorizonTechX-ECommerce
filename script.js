4c4a6a216746e0f63cb0d065e69f1b5a8fc0f36e
Tannu Kumari <tannusinha2006@gmail.com>
2026-07-03 10:27:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// HorizonTechX E-Commerce JavaScript
+
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to HorizonTechX E-Commerce Website!");
+});
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+    alert("Thank you for visiting HorizonTechX! Happy Shopping!");
+});
